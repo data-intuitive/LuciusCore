b83f728c012f3b6059c1c5225ab75453554cbc7b
Boris Shilov <21023619+borisofmuscovy@users.noreply.github.com>
2019-03-11 10:16:44 +0100

    working class + companion object, although cannot get PrivateMethodTester to work so commented those tests out for now

--- src/main/scala/com/dataintuitive/luciuscore/SignaturePair.scala
+++ src/main/scala/com/dataintuitive/luciuscore/SignaturePair.scala
@@ -3,58 +3,45 @@ package com.dataintuitive.luciuscore
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
-class SignaturePair(X: Vector[BigDecimal], Y: Vector[BigDecimal]) extends Serializable {
 
+object SignaturePair {
+  case class Coordinate(x: BigDecimal, y: BigDecimal)
+  case class Square(leftBottom: Coordinate, leftTop: Coordinate, rightBottom: Coordinate, rightTop: Coordinate)
+  case class BinnedCoordinate(theCoordinate: Coordinate, containingSquare: Square)
+}
+
+sealed class SignaturePair(X: Vector[BigDecimal], Y: Vector[BigDecimal]) extends Serializable {
+
+  import SignaturePair._
   /**
     * assumptions: We would like to reduce a scatter plot of many points to a heat map of squares, with each square
     * colored by how many points fall within it, in order to visualise very dense/large datasets.
     */
 
-  case class Coordinate(x: BigDecimal, y: BigDecimal)
-  case class XY(XY: Vector[BigDecimal])
-  case class Square(leftBottom: Coordinate, leftTop: Coordinate, rightBottom: Coordinate, rightTop: Coordinate)
-  case class BinnedCoordinate(theCoordinate: Coordinate, containingSquare: Square)
-
-  /**
-    * Checks if a point is inside an n-dimensional cube whose edges are strictly aligned with the axes
-    * @param point
-    * @param hypercubeEdges
-    * @return
-    */
-  def isInsideAxisAlignedHypercube(point: List[BigDecimal], hypercubeVertices: List[List[BigDecimal]]): Boolean = {
-    val dimensionIndex = point.indices
-    val verticesByDimension = dimensionIndex.map(index => hypercubeVertices.map{x => x(index)})
-    val extrema = verticesByDimension.map(x => (x.min, x.max))
-    val dimensionCheck = dimensionIndex
-      .map(index => (extrema(index)._1 <= point(index) &&  point(index) <= extrema(index)._2))
-    if (dimensionCheck.contains(false)) {
-      false
-    } else {
-      true
-    }
+  def squareToXY(square: Square): (Vector[BigDecimal], Vector[BigDecimal]) = {
+    (Vector(square.leftBottom.x, square.rightTop.x), Vector(square.leftBottom.y, square.rightTop.y))
   }
 
-  def isInsideSquare(point: Coordinate, square: Square): Boolean = {
-    val (xSquare, ySquare) = squareToXY(square)
-    if ((xSquare.min <= point.x && point.x <= xSquare.max) && (ySquare.min <= point.y && point.y <= ySquare.max)) true
-    else false
-  }
-
-  def squareToXY(square: Square): (List[BigDecimal], List[BigDecimal]) = {
-    (List(square.leftBottom.x, square.rightTop.x), List(square.leftBottom.y, square.rightTop.y))
-  }
-
-  def listsToTuples(xValues: List[BigDecimal], yValues: List[BigDecimal]): List[Coordinate] = {
+  def listsToTuples(xValues: Vector[BigDecimal], yValues: Vector[BigDecimal]): Vector[Coordinate] = {
     require(xValues.size == yValues.size, "Must be an x for every y.")
     (xValues, yValues).zipped.map(Coordinate)
   }
 
-  def tuplesToLists(XY: List[Coordinate]): (List[BigDecimal], List[BigDecimal]) = {
+  def tuplesToLists(XY: Vector[Coordinate]): (Vector[BigDecimal], Vector[BigDecimal]) = {
     XY.unzip{case Coordinate(x, y) => (x, y)}
   }
 
-  private def generateBottomLeftAndTopRight(xValues: List[BigDecimal], yValues: List[BigDecimal],
-                      partitionNum: BigDecimal): List[Seq[Coordinate]] = {
+  val XY: Vector[Coordinate] = this.listsToTuples(X, Y)
+
+  def isInsideSquare(point: Coordinate, square: Square): Boolean = {
+    val (xSquare, ySquare) = squareToXY(square)
+    if ((xSquare.min <= point.x && point.x <= xSquare.max) &&
+      (ySquare.min <= point.y && point.y <= ySquare.max)) true
+    else false
+  }
+
+  private def generateBottomLeftAndTopRight(xValues: Vector[BigDecimal], yValues: Vector[BigDecimal],
+                      partitionNum: BigDecimal): Vector[Vector[Coordinate]] = {
     if (partitionNum == 0) throw new IllegalArgumentException("Can not partition a dimension into 0 intervals.")
     val (xDiff, yDiff) = (xValues.max - xValues.min, yValues.max - yValues.min)
     val (xStepSize, yStepSize) =
@@ -62,28 +49,29 @@ class SignaturePair(X: Vector[BigDecimal], Y: Vector[BigDecimal]) extends Serial
     val (xSteps, ySteps) = (xValues.min to xValues.max by xStepSize toList, yValues.min to yValues.max by yStepSize toList)
     val xStepsSafe = if (!xSteps.contains(xValues.max)) xSteps.init ::: List(xValues.max) else xSteps
     val yStepsSafe = if (!ySteps.contains(yValues.max)) ySteps.init ::: List(yValues.max) else ySteps
-    val (xSlide, ySlide) = (xStepsSafe.iterator.sliding(2).toList, yStepsSafe.iterator.sliding(2).toList)
+    val (xSlide, ySlide) = (xStepsSafe.iterator.sliding(2).toVector, yStepsSafe.iterator.sliding(2).toVector)
     val asLists = xSlide.flatMap(xWindow => ySlide.map(yWindow => xWindow.zip(yWindow)))
-    asLists.map(twoPoints => twoPoints.map(aCoordinate => Coordinate(aCoordinate._1, aCoordinate._2)))
+    asLists.map(twoPoints => twoPoints.map(aCoordinate => Coordinate(aCoordinate._1, aCoordinate._2)).toVector)
   }
 
   private def imputeTopLeftAndBottomRight(bottomLeft: Coordinate, topRight: Coordinate): Square = {
     Square(bottomLeft, Coordinate(bottomLeft.x, topRight.y), Coordinate(topRight.x, bottomLeft.y), topRight)
   }
 
-  def generateSquares(xValues: List[BigDecimal], yValues: List[BigDecimal], partitionNum: BigDecimal): List[Square] = {
+  def generateSquares(xValues: Vector[BigDecimal], yValues: Vector[BigDecimal],
+                      partitionNum: BigDecimal): Vector[Square] = {
     generateBottomLeftAndTopRight(xValues, yValues, partitionNum).map(incompleteSquare =>
       imputeTopLeftAndBottomRight(incompleteSquare.head, incompleteSquare.last))
   }
 
-  def centroidMapper(squares: List[Square]): Map[Coordinate, Square] = {
+  def centroidMapper(squares: Vector[Square]): Map[Coordinate, Square] = {
     Map(squares.map{square =>
       (Coordinate(square.leftBottom.x + (square.rightBottom.x-square.leftBottom.x)/2,
         square.leftBottom.y + (square.leftTop.y - square.leftBottom.y)/2), square)
     }:_*)
   }
 
-  def centroidMapperRDD(sc: SparkContext, squares: List[Square]): RDD[(Coordinate, Square)] = {
+  def centroidMapperRDD(sc: SparkContext, squares: Vector[Square]): RDD[(Coordinate, Square)] = {
     val squaresRDD = sc.parallelize(squares)
     squaresRDD.map(square => (Coordinate(square.leftBottom.x + (square.rightBottom.x-square.leftBottom.x)/2,
         square.leftBottom.y + (square.leftTop.y - square.leftBottom.y)/2), square))
@@ -93,8 +81,7 @@ class SignaturePair(X: Vector[BigDecimal], Y: Vector[BigDecimal]) extends Serial
     val matchingSquareKeyList = squaresMap.keysIterator.takeWhile(square =>
       isInsideSquare(aCoordinate, squaresMap(square))).toList
     if (matchingSquareKeyList.size > 1) throw new IllegalArgumentException("Point belonging to more than one square!")
-    //TODO: define a proper Coordinate and Square classes which include a None coordinate definition to avoid this?
-    // otherwise this is a complete mess
+    // TODO: Option to replace sentinel value -999, -999
     val matchingSquareKey = matchingSquareKeyList.headOption.getOrElse(Coordinate(-999,-999))
     try {
     (aCoordinate, squaresMap.get(matchingSquareKey))
@@ -113,14 +100,13 @@ class SignaturePair(X: Vector[BigDecimal], Y: Vector[BigDecimal]) extends Serial
     (aCoordinate, MatchingSquare._2)
   }
 
-  def assignCoordinatesToSquares(sc: SparkContext, coordinateList: List[Coordinate],
-                                 squaresMap: Map[Coordinate, Square]): List[BinnedCoordinate] = {
-    val listOfBinnedCoordinates:List[(Coordinate, Square)] =  coordinateList.map{
+  def assignCoordinatesToSquares(sc: SparkContext, coordinateList: Vector[Coordinate],
+                                 squaresMap: Map[Coordinate, Square]): Vector[BinnedCoordinate] = {
+    val listOfBinnedCoordinates:Vector[(Coordinate, Square)] =  coordinateList.map{
       aCoordinate => whichSquareRDD(sc, aCoordinate, squaresMap)
     }
     listOfBinnedCoordinates.map(binnedCoordinateTuple =>
       BinnedCoordinate(binnedCoordinateTuple._1, binnedCoordinateTuple._2))
   }
-
 
 }
--- src/test/scala/com/dataintuitive/luciuscore/SignaturePairTest.scala
+++ src/test/scala/com/dataintuitive/luciuscore/SignaturePairTest.scala
@@ -1,61 +1,78 @@
 package com.dataintuitive.luciuscore
 
-import org.scalatest.FlatSpec
-import org.scalatest.PrivateMethodTester
+import org.scalatest.{FlatSpec, PrivateMethodTester}
 import com.dataintuitive.test.BaseSparkContextSpec
 import org.scalatest.Matchers._
 import com.dataintuitive.luciuscore.SignaturePair._
 
-class BinningFunctionsTest extends FlatSpec with BaseSparkContextSpec with PrivateMethodTester{
+class SignaturePairTest extends FlatSpec with BaseSparkContextSpec with PrivateMethodTester{
 
-  val X = List(BigDecimal(-1.0), BigDecimal(0.0), BigDecimal(1.0), BigDecimal(2.5))
-  val Y = List(BigDecimal(-0.5), BigDecimal(0.5), BigDecimal(2.0), BigDecimal(4.0))
+  val X = Vector(BigDecimal(-1.0), BigDecimal(0.0), BigDecimal(1.0), BigDecimal(2.5))
+  val Y = Vector(BigDecimal(-0.5), BigDecimal(0.5), BigDecimal(2.0), BigDecimal(4.0))
+  val pair1 = new SignaturePair(X, Y)
 
   "listsToTuples" should "correctly convert X vector and Y vector to (X, Y)" in {
-    assert(listsToTuples(X, Y) == List(Coordinate(-1.0, -0.5), Coordinate(0.0, 0.5),
+    assert(pair1.listsToTuples(X, Y) == Vector(Coordinate(-1.0, -0.5), Coordinate(0.0, 0.5),
       Coordinate(1.0, 2.0), Coordinate(2.5, 4.0)))
   }
 
   "tuplesToLists" should "correctly transform (X, Y) into two vectors" in {
-    assert(tuplesToLists(List(Coordinate(-1.0, -0.5), Coordinate(0.0, 0.5),
+    assert(pair1.tuplesToLists(Vector(Coordinate(-1.0, -0.5), Coordinate(0.0, 0.5),
       Coordinate(1.0, 2.0), Coordinate(2.5, 4.0))) == (X, Y))
   }
-
+  /*
   "generateBottomLeftAndTopRight" should "correctly return the coordinates of the bottom left and top right " +
     "corners of the smallest set of squares" in {
-    val BinningFuncs = new SignaturePair.
-    assert(PrivateMethod[BinningFunctions]('generateBottomLeftAndTopRight')(X, Y, 2) ==
-      List(List(Coordinate(-1.0,-0.5), Coordinate(0.75,1.75)),
-        List(Coordinate(-1.0,1.75), Coordinate(0.75,4.00)),
-        List(Coordinate(0.75,-0.5), Coordinate(2.50,1.75)),
-        List(Coordinate(0.75,1.75), Coordinate(2.50,4.00))))
+    val generateBottomLeftAndTopRight = PrivateMethod[Vector[Vector[Coordinate]]]('generateBottomLeftAndTopRight)
+    val res1 = SignaturePair invokePrivate generateBottomLeftAndTopRight(X, Y, BigDecimal(2.0))
+    assert(res1 ===
+      Vector(Vector(Coordinate(-1.0,-0.5), Coordinate(0.75,1.75)),
+        Vector(Coordinate(-1.0,1.75), Coordinate(0.75,4.00)),
+        Vector(Coordinate(0.75,-0.5), Coordinate(2.50,1.75)),
+        Vector(Coordinate(0.75,1.75), Coordinate(2.50,4.00))))
   }
 
+  "generateBottomLeftAndTopRight" should "create a vector" in {
+    val partitions = BigDecimal(2.0)
+    val generateBottomLeftAndTopRight = PrivateMethod[Vector[Vector[Coordinate]]]('generateBottomLeftAndTopRight)
+    val incompleteSquares = SignaturePair invokePrivate generateBottomLeftAndTopRight(X, Y, partitions)
+    incompleteSquares shouldBe a [List[_]]
+  }
+
+
   "imputeTopLeftAndBottomRight" should "correctly impute a simple square" in {
+    val imputeTopLeftAndBottomRight = PrivateMethod[Square]('imputeTopLeftAndBottomRight)
     val bottomLeft = Coordinate(BigDecimal(-1.0), BigDecimal(-1.0))
     val topRight = Coordinate(BigDecimal(0.75), BigDecimal(0.75))
-    assert(imputeTopLeftAndBottomRight(bottomLeft, topRight) ==
+    val res1= SignaturePair invokePrivate imputeTopLeftAndBottomRight(bottomLeft, topRight)
+    assert(res1 ===
       Square(Coordinate(BigDecimal(-1.0), BigDecimal(-1.0)), Coordinate(BigDecimal(-1.0), BigDecimal(0.75)),
       Coordinate(BigDecimal(0.75), BigDecimal(-1.0)), Coordinate(BigDecimal(0.75), BigDecimal(0.75))))
   }
 
-  val partitions = 2
-  val incompleteSquares = generateBottomLeftAndTopRight(X, Y, partitions)
-  incompleteSquares shouldBe a [List[_]]
-  val completeSquares = incompleteSquares.map(twoCoordinates => imputeTopLeftAndBottomRight(twoCoordinates(0), twoCoordinates(1)))
-  completeSquares shouldBe a [List[_]]
-  completeSquares.head shouldBe a [Square]
-  completeSquares.size shouldBe 4
+  "imputeTopLeftAndBottomRight" should "correctly impute squares" in {
+    val partitions = 2
+    val generateBottomLeftAndTopRight = PrivateMethod[Vector[Seq[Coordinate]]]('generateBottomLeftAndTopRight)
+    val incompleteSquares = SignaturePair invokePrivate generateBottomLeftAndTopRight(X, Y, partitions)
+    val imputeTopLeftAndBottomRight = PrivateMethod[Square]('imputeTopLeftAndBottomRight)
+    /*val completeSquares = incompleteSquares.map(twoCoordinates =>
+      SignaturePair invokePrivate imputeTopLeftAndBottomRight(twoCoordinates(0), twoCoordinates(1)))
+    completeSquares shouldBe a [List[_]]
+    completeSquares.head shouldBe a [Square]
+    completeSquares.size shouldBe 4*/
+  }
+  */
 
   "centroidMapper" should "correctly create a Map of square centers and the associated squares" in {
     val square1 = Square(Coordinate(-1.0, 0.75), Coordinate(-1.0, 2.5), Coordinate(0.75, 0.75), Coordinate(0.75, 2.5))
     val square2 = Square(Coordinate(-1.0, -1.0), Coordinate(-1.0, 0.75), Coordinate(0.75, -1.0), Coordinate(0.75, 0.75))
-    assert(centroidMapper(List(square1, square2)) == Map(Coordinate(-0.125, 1.625) -> square1, Coordinate(-0.125, -0.125) -> square2))
+    assert(pair1.centroidMapper(Vector(square1, square2)) ==
+      Map(Coordinate(-0.125, 1.625) -> square1, Coordinate(-0.125, -0.125) -> square2))
   }
 
   "generateSquares" should "correctly generate squares when each dim has two partitions" in {
-    val squares = generateSquares(X, Y, 2)
-    assert(squares == List(
+    val squares = pair1.generateSquares(X, Y, 2)
+    assert(squares == Vector(
       Square(Coordinate(-1.0,-0.5),Coordinate(-1.0,1.75),Coordinate(0.75,-0.5),Coordinate(0.75,1.75)),
       Square(Coordinate(-1.0,1.75),Coordinate(-1.0,4.00),Coordinate(0.75,1.75),Coordinate(0.75,4.00)),
       Square(Coordinate(0.75,-0.5),Coordinate(0.75,1.75),Coordinate(2.50,-0.5),Coordinate(2.50,1.75)),
@@ -65,7 +82,7 @@ class BinningFunctionsTest extends FlatSpec with BaseSparkContextSpec with Priva
   "centroidMapperRDD" should "correctly create a Map of square centers and the associated squares" in {
     val square1 = Square(Coordinate(-1.0, 0.75), Coordinate(-1.0, 2.5), Coordinate(0.75, 0.75), Coordinate(0.75, 2.5))
     val square2 = Square(Coordinate(-1.0, -1.0), Coordinate(-1.0, 0.75), Coordinate(0.75, -1.0), Coordinate(0.75, 0.75))
-    val pairsRDD = centroidMapperRDD(sc, List(square1, square2))
+    val pairsRDD = pair1.centroidMapperRDD(sc, Vector(square1, square2))
     assert(pairsRDD.collectAsMap == Map(Coordinate(-0.125, 1.625) -> square1, Coordinate(-0.125, -0.125) -> square2))
   }
 
@@ -74,7 +91,7 @@ class BinningFunctionsTest extends FlatSpec with BaseSparkContextSpec with Priva
     val square2 = Square(Coordinate(-1.0, -1.0), Coordinate(-1.0, 0.75), Coordinate(0.75, -1.0), Coordinate(0.75, 0.75))
     val squareCentroidMap = Map(Coordinate(-0.125, 1.625) -> square1, Coordinate(-0.125, -0.125) -> square2)
     val coordinateInsideSquare1 = Coordinate(0, 1.5)
-    assert(whichSquare(coordinateInsideSquare1, squareCentroidMap) == (coordinateInsideSquare1, Option(square1)))
+    assert(pair1.whichSquare(coordinateInsideSquare1, squareCentroidMap) == (coordinateInsideSquare1, Option(square1)))
   }
 
   "whichSquare" should "gracefully return None if coordinate is outside a square" in {
@@ -82,7 +99,7 @@ class BinningFunctionsTest extends FlatSpec with BaseSparkContextSpec with Priva
     val square2 = Square(Coordinate(-1.0, -1.0), Coordinate(-1.0, 0.75), Coordinate(0.75, -1.0), Coordinate(0.75, 0.75))
     val squareCentroidMap = Map(Coordinate(-0.125, 1.625) -> square1, Coordinate(-0.125, -0.125) -> square2)
     val coordinateOutsideSquare1 = Coordinate(-6, -9)
-    assert(whichSquare(coordinateOutsideSquare1, squareCentroidMap) == (coordinateOutsideSquare1, None))
+    assert(pair1.whichSquare(coordinateOutsideSquare1, squareCentroidMap) == (coordinateOutsideSquare1, None))
   }
 
   "whichSquareRDD" should "correctly determine a coordinate is inside a square" in {
@@ -90,16 +107,16 @@ class BinningFunctionsTest extends FlatSpec with BaseSparkContextSpec with Priva
     val square2 = Square(Coordinate(-1.0, -1.0), Coordinate(-1.0, 0.75), Coordinate(0.75, -1.0), Coordinate(0.75, 0.75))
     val squareCentroidMap = Map(Coordinate(-0.125, 1.625) -> square1, Coordinate(-0.125, -0.125) -> square2)
     val coordinateInsideSquare1 = Coordinate(0, 1.5)
-    assert(whichSquareRDD(sc, coordinateInsideSquare1, squareCentroidMap) == (coordinateInsideSquare1, square1))
+    assert(pair1.whichSquareRDD(sc, coordinateInsideSquare1, squareCentroidMap) == (coordinateInsideSquare1, square1))
   }
 
   "assignCoordinatesToSquares" should "correctly assign a group of points to a group of squares" in {
     val square1 = Square(Coordinate(-1.0, 0.75), Coordinate(-1.0, 2.5), Coordinate(0.75, 0.75), Coordinate(0.75, 2.5))
     val square2 = Square(Coordinate(-1.0, -1.0), Coordinate(-1.0, 0.75), Coordinate(0.75, -1.0), Coordinate(0.75, 0.75))
     val squareCentroidMap = Map(Coordinate(-0.125, 1.625) -> square1, Coordinate(-0.125, -0.125) -> square2)
-    val coordList = List(Coordinate(0, 1.5), Coordinate(0, 0))
-    assert(assignCoordinatesToSquares(sc, coordList, squareCentroidMap) ==
-      List(BinnedCoordinate(Coordinate(0,1.5),
+    val coordList = Vector(Coordinate(0, 1.5), Coordinate(0, 0))
+    assert(pair1.assignCoordinatesToSquares(sc, coordList, squareCentroidMap) ==
+      Vector(BinnedCoordinate(Coordinate(0,1.5),
         Square(Coordinate(-1.0,0.75),Coordinate(-1.0,2.5),Coordinate(0.75,0.75),Coordinate(0.75,2.5))),
         BinnedCoordinate(Coordinate(0,0),
           Square(Coordinate(-1.0,-1.0),Coordinate(-1.0,0.75),Coordinate(0.75,-1.0),Coordinate(0.75,0.75)))))
