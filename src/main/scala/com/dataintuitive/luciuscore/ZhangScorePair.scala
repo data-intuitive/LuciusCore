dd7832032c963498af8f5c7e38d342662052d742
Boris Shilov <21023619+borisofmuscovy@users.noreply.github.com>
2019-03-12 14:40:59 +0100

    Coordinate -> Point

--- src/main/scala/com/dataintuitive/luciuscore/ZhangScorePair.scala
+++ src/main/scala/com/dataintuitive/luciuscore/ZhangScorePair.scala
@@ -6,29 +6,29 @@ import org.apache.spark.rdd.RDD
 
 object ZhangScorePair {
 
-  case class Coordinate(x: BigDecimal, y: BigDecimal) {
+  case class Point(x: BigDecimal, y: BigDecimal) {
     def toTuple: (BigDecimal, BigDecimal) = {
       (this.x, this.y)
     }
   }
 
-  case class Square(leftBottom: Coordinate, leftTop: Coordinate, rightBottom: Coordinate, rightTop: Coordinate) {
-    val center = Coordinate(this.leftBottom.x + (this.rightBottom.x-this.leftBottom.x)/2,
+  case class Square(leftBottom: Point, leftTop: Point, rightBottom: Point, rightTop: Point) {
+    val center = Point(this.leftBottom.x + (this.rightBottom.x-this.leftBottom.x)/2,
       this.leftBottom.y + (this.leftTop.y - this.leftBottom.y)/2)
     def toXY: (Vector[BigDecimal], Vector[BigDecimal]) = {
       (Vector(this.leftBottom.x, this.rightTop.x), Vector(this.leftBottom.y, this.rightTop.y))
     }
   }
 
-  def isInsideSquare(point: Coordinate, square: Square): Boolean = {
+  def isInsideSquare(point: Point, square: Square): Boolean = {
     val (xSquare, ySquare) = square.toXY
     if ((xSquare.min <= point.x && point.x <= xSquare.max) &&
       (ySquare.min <= point.y && point.y <= ySquare.max)) true
     else false
   }
 
-  def whichSquare(aCoordinate: Coordinate, squares: Vector[Square]):
-  Option[(Coordinate, Square)] = {
+  def whichSquare(aCoordinate: Point, squares: Vector[Square]):
+  Option[(Point, Square)] = {
     val matchingSquares = squares.filter(isInsideSquare(aCoordinate, _))
     if (matchingSquares.length != 1) None
     else Some {
@@ -36,17 +36,17 @@ object ZhangScorePair {
     }
   }
 
-  def assignCoordinatesToSquares(sc: SparkContext, coordinateList: Vector[Coordinate],
-                                 squares: Vector[Square]): RDD[(Square,Coordinate)] = {
+  def assignCoordinatesToSquares(sc: SparkContext, coordinateList: Vector[Point],
+                                 squares: Vector[Square]): RDD[(Square,Point)] = {
     sc.parallelize(coordinateList)
       .map{aCoordinate => whichSquare(aCoordinate, squares)}
       .map(binnedCoordinateTuple => (binnedCoordinateTuple.get._2, binnedCoordinateTuple.get._1 ))
   }
 
-  def squaresWithAllPoints(sc: SparkContext, binnedCoords: RDD[(Square, Coordinate)]): RDD[(Square, Vector[Coordinate])] = {
-    val addFunction = (vector1: Vector[Coordinate], coord: Coordinate) => vector1++Vector(coord)
-    val mergeFunction = (vector1: Vector[Coordinate], vector2: Vector[Coordinate]) => vector1++vector2
-    binnedCoords.aggregateByKey(Vector[Coordinate]())(addFunction, mergeFunction)
+  def squaresWithAllPoints(sc: SparkContext, binnedCoords: RDD[(Square, Point)]): RDD[(Square, Vector[Point])] = {
+    val addFunction = (vector1: Vector[Point], coord: Point) => vector1++Vector(coord)
+    val mergeFunction = (vector1: Vector[Point], vector2: Vector[Point]) => vector1++vector2
+    binnedCoords.aggregateByKey(Vector[Point]())(addFunction, mergeFunction)
   }
 
 }
@@ -54,7 +54,7 @@ object ZhangScorePair {
 sealed class ZhangScorePair(X: Vector[BigDecimal], Y: Vector[BigDecimal]) extends Serializable {
 
   import ZhangScorePair._
-  val XY: Vector[Coordinate] = (this.X, this.Y).zipped.map(Coordinate)
+  val XY: Vector[Point] = (this.X, this.Y).zipped.map(Point)
 
   def generateSquares(partitionNum: BigDecimal): Option[Vector[Square]] = {
     if (partitionNum <= 0) None
@@ -67,10 +67,10 @@ sealed class ZhangScorePair(X: Vector[BigDecimal], Y: Vector[BigDecimal]) extend
       val yStepsSafe = if (!ySteps.contains(this.Y.max)) ySteps.init ::: List(this.Y.max) else ySteps
       val (xSlide, ySlide) = (xStepsSafe.iterator.sliding(2).toVector, yStepsSafe.iterator.sliding(2).toVector)
       xSlide.flatMap(xWindow => ySlide.map(yWindow => xWindow.zip(yWindow)))
-        .map(twoPoints => twoPoints.map(aCoordinate => Coordinate(aCoordinate._1, aCoordinate._2)).toVector)
+        .map(twoPoints => twoPoints.map(aCoordinate => Point(aCoordinate._1, aCoordinate._2)).toVector)
         .map(incompleteSquare =>
-        Square(incompleteSquare.head, Coordinate(incompleteSquare.head.x, incompleteSquare.last.y),
-          Coordinate(incompleteSquare.last.x, incompleteSquare.head.y), incompleteSquare.last))
+        Square(incompleteSquare.head, Point(incompleteSquare.head.x, incompleteSquare.last.y),
+          Point(incompleteSquare.last.x, incompleteSquare.head.y), incompleteSquare.last))
     }
   }
 
--- src/test/scala/com/dataintuitive/luciuscore/ZhangScorePairTest.scala
+++ src/test/scala/com/dataintuitive/luciuscore/ZhangScorePairTest.scala
@@ -1,8 +1,8 @@
 package com.dataintuitive.luciuscore
 
 import org.scalatest.{FlatSpec, PrivateMethodTester}
-import com.dataintuitive.test.BaseSparkContextSpec
 import org.scalatest.Matchers._
+import com.dataintuitive.test.BaseSparkContextSpec
 import com.dataintuitive.luciuscore.ZhangScorePair._
 
 class ZhangScorePairTest extends FlatSpec with BaseSparkContextSpec with PrivateMethodTester{
@@ -12,63 +12,63 @@ class ZhangScorePairTest extends FlatSpec with BaseSparkContextSpec with Private
   val pair1 = new ZhangScorePair(X, Y)
 
   "ZhangScorePair primary constructor" should "correctly convert X vector and Y vector to (X, Y)" in {
-    assert(pair1.XY == Vector(Coordinate(-1.0, -0.5), Coordinate(0.0, 0.5),
-      Coordinate(1.0, 2.0), Coordinate(2.5, 4.0)))
+    assert(pair1.XY == Vector(Point(-1.0, -0.5), Point(0.0, 0.5),
+      Point(1.0, 2.0), Point(2.5, 4.0)))
   }
 
   "group of coordinates" should "correctly transform from (X, Y) into two vectors using toTuples" in {
-    val coordVec = Vector(Coordinate(-1.0, -0.5), Coordinate(0.0, 0.5),
-      Coordinate(1.0, 2.0), Coordinate(2.5, 4.0))
+    val coordVec = Vector(Point(-1.0, -0.5), Point(0.0, 0.5),
+      Point(1.0, 2.0), Point(2.5, 4.0))
     assert(coordVec.map(_.toTuple).unzip == (X, Y))
   }
 
   "Square case class" should "correctly compute centers " in {
-    val square1 = Square(Coordinate(-1.0, 0.75), Coordinate(-1.0, 2.5), Coordinate(0.75, 0.75), Coordinate(0.75, 2.5))
-    val square2 = Square(Coordinate(-1.0, -1.0), Coordinate(-1.0, 0.75), Coordinate(0.75, -1.0), Coordinate(0.75, 0.75))
+    val square1 = Square(Point(-1.0, 0.75), Point(-1.0, 2.5), Point(0.75, 0.75), Point(0.75, 2.5))
+    val square2 = Square(Point(-1.0, -1.0), Point(-1.0, 0.75), Point(0.75, -1.0), Point(0.75, 0.75))
     assert(Vector(square1.center, square2.center) ==
-      Vector(Coordinate(-0.125, 1.625), Coordinate(-0.125, -0.125)))
+      Vector(Point(-0.125, 1.625), Point(-0.125, -0.125)))
   }
 
   "generateSquares" should "correctly generate squares when each dim has two partitions" in {
     val squares = pair1.generateSquares(2).get
     assert(squares == Vector(
-      Square(Coordinate(-1.0,-0.5),Coordinate(-1.0,1.75),Coordinate(0.75,-0.5),Coordinate(0.75,1.75)),
-      Square(Coordinate(-1.0,1.75),Coordinate(-1.0,4.00),Coordinate(0.75,1.75),Coordinate(0.75,4.00)),
-      Square(Coordinate(0.75,-0.5),Coordinate(0.75,1.75),Coordinate(2.50,-0.5),Coordinate(2.50,1.75)),
-      Square(Coordinate(0.75,1.75),Coordinate(0.75,4.00),Coordinate(2.50,1.75),Coordinate(2.50,4.00))))
+      Square(Point(-1.0,-0.5),Point(-1.0,1.75),Point(0.75,-0.5),Point(0.75,1.75)),
+      Square(Point(-1.0,1.75),Point(-1.0,4.00),Point(0.75,1.75),Point(0.75,4.00)),
+      Square(Point(0.75,-0.5),Point(0.75,1.75),Point(2.50,-0.5),Point(2.50,1.75)),
+      Square(Point(0.75,1.75),Point(0.75,4.00),Point(2.50,1.75),Point(2.50,4.00))))
   }
 
   "whichSquare" should "gracefully return None if coordinate is outside a square" in {
-    val square1 = Square(Coordinate(-1.0, 0.75), Coordinate(-1.0, 2.5), Coordinate(0.75, 0.75), Coordinate(0.75, 2.5))
-    val square2 = Square(Coordinate(-1.0, -1.0), Coordinate(-1.0, 0.75), Coordinate(0.75, -1.0), Coordinate(0.75, 0.75))
-    val coordinateOutsideSquare1 = Coordinate(-6, -9)
+    val square1 = Square(Point(-1.0, 0.75), Point(-1.0, 2.5), Point(0.75, 0.75), Point(0.75, 2.5))
+    val square2 = Square(Point(-1.0, -1.0), Point(-1.0, 0.75), Point(0.75, -1.0), Point(0.75, 0.75))
+    val coordinateOutsideSquare1 = Point(-6, -9)
     assert(whichSquare(coordinateOutsideSquare1, Vector(square1, square2)).isEmpty)
   }
 
   "whichSquare" should "correctly determine a coordinate is inside a square" in {
-    val square1 = Square(Coordinate(-1.0, 0.75), Coordinate(-1.0, 2.5), Coordinate(0.75, 0.75), Coordinate(0.75, 2.5))
-    val square2 = Square(Coordinate(-1.0, -1.0), Coordinate(-1.0, 0.75), Coordinate(0.75, -1.0), Coordinate(0.75, 0.75))
-    val coordinateInsideSquare1 = Coordinate(0, 1.5)
+    val square1 = Square(Point(-1.0, 0.75), Point(-1.0, 2.5), Point(0.75, 0.75), Point(0.75, 2.5))
+    val square2 = Square(Point(-1.0, -1.0), Point(-1.0, 0.75), Point(0.75, -1.0), Point(0.75, 0.75))
+    val coordinateInsideSquare1 = Point(0, 1.5)
     assert(whichSquare(coordinateInsideSquare1, Vector(square1, square2)).get == (coordinateInsideSquare1, square1))
   }
 
   "assignCoordinatesToSquares" should "correctly assign a group of points to a group of squares" in {
-    val square1 = Square(Coordinate(-1.0, 0.75), Coordinate(-1.0, 2.5), Coordinate(0.75, 0.75), Coordinate(0.75, 2.5))
-    val square2 = Square(Coordinate(-1.0, -1.0), Coordinate(-1.0, 0.75), Coordinate(0.75, -1.0), Coordinate(0.75, 0.75))
-    val coordList = Vector(Coordinate(0, 1.5), Coordinate(0, 0))
+    val square1 = Square(Point(-1.0, 0.75), Point(-1.0, 2.5), Point(0.75, 0.75), Point(0.75, 2.5))
+    val square2 = Square(Point(-1.0, -1.0), Point(-1.0, 0.75), Point(0.75, -1.0), Point(0.75, 0.75))
+    val coordList = Vector(Point(0, 1.5), Point(0, 0))
     assert(assignCoordinatesToSquares(sc, coordList, Vector(square1, square2)).collect.toVector ==
-      Vector((Square(Coordinate(-1.0,0.75),Coordinate(-1.0,2.5),Coordinate(0.75,0.75),Coordinate(0.75,2.5)), Coordinate(0,1.5)),
-        (Square(Coordinate(-1.0,-1.0),Coordinate(-1.0,0.75),Coordinate(0.75,-1.0),Coordinate(0.75,0.75)), Coordinate(0,0))))
+      Vector((Square(Point(-1.0,0.75),Point(-1.0,2.5),Point(0.75,0.75),Point(0.75,2.5)), Point(0,1.5)),
+        (Square(Point(-1.0,-1.0),Point(-1.0,0.75),Point(0.75,-1.0),Point(0.75,0.75)), Point(0,0))))
   }
 
   "squaresWithAllPoints" should "correctly aggregate every coordinate into vectors in their squares" in {
-    val squaresAndPoints = Vector((Square(Coordinate(-1.0,0.75),Coordinate(-1.0,2.5),Coordinate(0.75,0.75),Coordinate(0.75,2.5)), Coordinate(0,1.5)),
-    (Square(Coordinate(-1.0,-1.0),Coordinate(-1.0,0.75),Coordinate(0.75,-1.0),Coordinate(0.75,0.75)), Coordinate(0,0)),
-      (Square(Coordinate(-1.0,-1.0),Coordinate(-1.0,0.75),Coordinate(0.75,-1.0),Coordinate(0.75,0.75)), Coordinate(0.01,0.01)))
+    val squaresAndPoints = Vector((Square(Point(-1.0,0.75),Point(-1.0,2.5),Point(0.75,0.75),Point(0.75,2.5)), Point(0,1.5)),
+    (Square(Point(-1.0,-1.0),Point(-1.0,0.75),Point(0.75,-1.0),Point(0.75,0.75)), Point(0,0)),
+      (Square(Point(-1.0,-1.0),Point(-1.0,0.75),Point(0.75,-1.0),Point(0.75,0.75)), Point(0.01,0.01)))
     val squaresAndPointsRDD = sc.parallelize(squaresAndPoints)
     val res1 = squaresWithAllPoints(sc, squaresAndPointsRDD).collectAsMap
-    assert(res1 ==  Map(Square(Coordinate(-1.0,0.75),Coordinate(-1.0,2.5),Coordinate(0.75,0.75),Coordinate(0.75,2.5)) -> Vector(Coordinate(0,1.5)),
-      Square(Coordinate(-1.0,-1.0),Coordinate(-1.0,0.75),Coordinate(0.75,-1.0),Coordinate(0.75,0.75)) -> Vector(Coordinate(0,0),Coordinate(0.01,0.01))))
+    assert(res1 ==  Map(Square(Point(-1.0,0.75),Point(-1.0,2.5),Point(0.75,0.75),Point(0.75,2.5)) -> Vector(Point(0,1.5)),
+      Square(Point(-1.0,-1.0),Point(-1.0,0.75),Point(0.75,-1.0),Point(0.75,0.75)) -> Vector(Point(0,0),Point(0.01,0.01))))
   }
 
 }
