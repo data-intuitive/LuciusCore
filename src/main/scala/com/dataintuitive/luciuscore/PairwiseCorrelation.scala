e35bf0e7cd366004a86d13c09809a46a7669cce0
Boris Shilov <borisshilov@agr-wrk-dict13.agr.ad10.intern.kuleuven.ac.be>
2019-05-09 15:10:05 +0200

    Working starting implementation using the type class pattern

--- src/main/scala/com/dataintuitive/luciuscore/PairwiseCorrelation.scala
+++ src/main/scala/com/dataintuitive/luciuscore/PairwiseCorrelation.scala
@@ -6,16 +6,20 @@ object PairwiseCorrelation extends Serializable {
 
 
 
-  implicit class Point[A](val tuple: (A, A))(implicit evidence: A => Numeric[A]) {
+  implicit class Point[A](val tuple: (A, A))(implicit num: Numeric[A]) {
     val x: A = this.tuple._1
     val y: A = this.tuple._2
   }
+  /**
+  implicit def Tuple2Point[A](tuple: (A, A))(implicit evidence: A => Numeric[A]): Point[A] = {
+    new Point(tuple)
+  }**/
 
   implicit class Bin (val bin: (Int, Int)) {???}
 
   case class AxisAlignedSquare[A](leftBottom: Point[A], leftTop: Point[A], rightBottom: Point[A], rightTop: Point[A])
-                            (implicit orderedNumeric: A => Ordered[A]) {
-    import PairwiseCorrelation.Point
+                            (implicit ordering: Ordering[A]) {
+    import ordering._
     require{ leftBottom.x == leftTop.x &&
       rightBottom.x == rightTop.x &&
       leftBottom.y == rightBottom.y &&
@@ -25,7 +29,7 @@ object PairwiseCorrelation extends Serializable {
     leftBottom.y <= leftTop.y &&
     rightBottom.y <= rightTop.y} // check top corners greater than bottoms, rights greater than lefts
 
-    def this(leftBottom: Point[A], rightTop: Point[A])(implicit orderedNumeric: A => Ordered[A]) = this(leftBottom,
+    def this(leftBottom: Point[A], rightTop: Point[A])(implicit num: Numeric[A], ordering: Ordering[A]) = this(leftBottom,
       (leftBottom.x, rightTop.y), (rightTop.x, leftBottom.y), rightTop)
   }
 
--- src/test/scala/com/dataintuitive/luciuscore/PairwiseCorrelationTest.scala
+++ src/test/scala/com/dataintuitive/luciuscore/PairwiseCorrelationTest.scala
@@ -3,16 +3,48 @@ package com.dataintuitive.luciuscore
 import com.dataintuitive.luciuscore.PairwiseCorrelation._
 import org.scalatest.FlatSpec
 
+import scala.runtime.RichInt
+
 class PairwiseCorrelationTest extends FlatSpec {
-
-  "implicit class Point" should "instantiate correctly from a tuple" in {
-
+/**
+  "Square constructor" should "correctly generate an integer square" in {
+    implicit def Tuple2Point[A](tuple: (A, A))(implicit evidence: Numeric[A]): Point[A] = {
+      new Point(tuple)
+    }
+    val square = new AxisAlignedSquare[Int]((1, 1), (1, 2), (2, 1), (2, 2))
+  }
+  "Square constructor" should "correctly generate an integer square" in {
+    implicit def IntTuple2Point[Int](tuple: (Int, Int)): Point[Int] = {
+      implicit def Numerically(i:Int): Numeric[Int] = {Numeric(Int)}
+      new Point(tuple)
+    }
+    val square = new AxisAlignedSquare[Int]((1, 1), (1, 2), (2, 1), (2, 2))
   }
 
+
   "Square constructor" should "correctly generate an integer square" in {
-    import com.dataintuitive.luciuscore.PairwiseCorrelation.Point
-    implicit val tuples = Numeric[Int]
-    AxisAlignedSquare((1, 1), (1, 2), (2, 1), (2, 2))
+    val square = new AxisAlignedSquare[Int](Point[Int](1, 1), Point[Int](1, 2), Point[Int](2, 1), Point[Int](2, 2))
+  }
+
+
+  "Square constructor" should "correctly generate an integer square from just two elements" in {
+    new AxisAlignedSquare[Int]((1, 1), (2, 2))
+  }
+
+  "Square constructor" should "correctly generate an integer square from just two elements" in {
+    AxisAlignedSquare((1, 1), (2, 2))
+  }**/
+
+  "Square constructor" should "correctly generate an integer square" in {
+    val square = new AxisAlignedSquare[Int]((1, 1), (1, 2), (2, 1), (2, 2))
+    assert(square.leftBottom.tuple == (1, 1) && square.leftTop.tuple == (1, 2)
+      && square.rightBottom.tuple == (2, 1) & square.rightTop.tuple == (2, 2))
+  }
+
+  "Square constructor" should "correctly generate an integer square from only a partial constructor" in {
+    val square = new AxisAlignedSquare[Int]((1, 1), (2, 2))
+    assert(square.leftBottom.tuple == (1, 1) && square.leftTop.tuple == (1, 2)
+      && square.rightBottom.tuple == (2, 1) & square.rightTop.tuple == (2, 2))
   }
 
 }
