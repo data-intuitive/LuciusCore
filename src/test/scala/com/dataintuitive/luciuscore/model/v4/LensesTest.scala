1df5f47b8ea2ea700805b4b872f767d9d7e13d4a
Toni Verbeiren <toni.verbeiren@gmail.com>
2021-03-12 09:28:10 +0100

    Adapt tests - WIP

--- src/test/scala/com/dataintuitive/luciuscore/model/v4/LensesTest.scala
+++ src/test/scala/com/dataintuitive/luciuscore/model/v4/LensesTest.scala
@@ -8,7 +8,7 @@ class LensesTest extends AnyFlatSpec {
 
   info("Test v4 Model")
 
-  val p = Perturbation("123", trt = TRT_CP(name = "test123", concentration = "conc1"))
+  val p = Perturbation("123")
 
   "Minimal Instantiation" should "simply work" in {
     assert(p.id === "123")
--- src/test/scala/com/dataintuitive/luciuscore/model/v4/ModelTest.scala
+++ src/test/scala/com/dataintuitive/luciuscore/model/v4/ModelTest.scala
@@ -6,10 +6,61 @@ class ModelTestv4 extends AnyFlatSpec {
 
   info("Test v4 Model")
 
-  val p = Perturbation("123", trt = TRT_CP(name = "test123", concentration = "conc1"))
+  val p = Perturbation("123")
 
   "Minimal Instantiation" should "simply work" in {
     assert(p.id === "123")
+    assert(p.trtType === "empty")
+    assert(p.trt === TRT_EMPTY)
+    assert(p.trt_cp === None)
+  }
+
+  val trtGenericCp =
+    TRT_GENERIC(
+      "trt_cp",
+      "pertId",
+      "pertName",
+      Some("inchikey"),
+      Some("smiles"),
+      Some("pubchem"),
+      Some("1"),
+      Some("ml"),
+      Some("4"),
+      Some("m")
+    )
+
+  val trtGenericLig =
+    TRT_GENERIC(
+      "trt_lig",
+      "pertId",
+      "pertName",
+      None,
+      None,
+      None,
+      Some("2"),
+      Some("ml"),
+      Some("1"),
+      Some("m")
+    )
+
+  val cp = Perturbation(
+    "pid",
+    Information(),
+    Nil,
+    trtGenericCp,
+    Nil
+  )
+  val lig = Perturbation(
+    "pid",
+    Information(),
+    Nil,
+    trtGenericLig,
+    Nil
+  )
+
+  "Full instantiation" should "automatically create the correct entries" in {
+    assert(cp.trt === trtGenericCp)
+    assert(cp.trt_cp === ModelFunctions.convertToSpecific(trtGenericCp))
   }
 
 }
